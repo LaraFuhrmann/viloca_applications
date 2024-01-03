fa3d0232cc8945e193513d54456dd2be505491e5
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-01-03 09:53:17 +0100

    fix sample download script

--- hiv_clinical/resources/samples/download_samples.sh
+++ hiv_clinical/resources/samples/download_samples.sh
@@ -4,10 +4,17 @@ module load sra-toolkit/2.10.5
 
 mkdir -p CAP217/week_358
 
+cd CAP217/week_358
+
 fastq-dump -O raw_data --split-e  SRR9588785
+
+mv raw_data/SRR9588785_1.fastq raw_data/SRR9588785_R1.fastq
+mv raw_data/SRR9588785_2.fastq raw_data/SRR9588785_R2.fastq
+
 
 cd ../
 
-mkdir -p CAP217/week_112
+fastq-dump -O week_112/raw_data --split-e  SRR9588774
 
-fastq-dump -O raw_data --split-e  SRR9588774
+mv week_112/raw_data/SRR9588774_1.fastq week_112/raw_data/SRR9588774_R1.fastq
+mv week_112/raw_data/SRR9588774_2.fastq week_112/raw_data/SRR9588774_R2.fastq 
