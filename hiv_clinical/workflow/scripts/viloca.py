6045a3235e36494923bc185645cbf7fa0d20283a
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-01-04 10:31:39 +0100

    correct rule arguments for run_viloca rule

--- hiv_clinical/workflow/scripts/viloca.py
+++ hiv_clinical/workflow/scripts/viloca.py
@@ -1,14 +1,14 @@
 import subprocess
 from pathlib import Path
 
-def main(fname_bam, fname_reference, fname_insert_bed, fname_results_snv, fname_results_csv, dname_work):
+def main(fname_bam, fname_reference, fname_results_snv, fname_results_csv, dname_work):
 
     alpha = 0.000001
     n_max_haplotypes = 100
     n_mfa_starts = 1
-    print("before create direcotyr")
+
     dname_work.mkdir(parents=True, exist_ok=True)
-    print("before run viloca")
+
     subprocess.run(
         [
             "shorah",
@@ -34,15 +34,14 @@ def main(fname_bam, fname_reference, fname_insert_bed, fname_results_snv, fname_
     )
 
     (dname_work / "snv" / "SNVs_0.010000_final.vcf").rename(fname_results_snv)
-    (dname_work / "snv" / "cooccurring_mutations.csv").rename(fname_results_snv)
+    (dname_work / "snv" / "cooccurring_mutations.csv").rename(fname_results_csv)
 
 
 if __name__ == "__main__":
     main(
         Path(snakemake.input.fname_bam),
         Path(snakemake.input.fname_reference),
-        Path(snakemake.input.fname_insert_bed),
-        Path(snakemake.output.fname_vcf),
+        Path(snakemake.output.fname_snv_vcf),
         Path(snakemake.output.fname_csv),
         Path(snakemake.output.dname_work),
     )
