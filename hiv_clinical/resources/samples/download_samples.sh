b88aa9195fe697472c34069114c99d8f2f4d117e
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-01-10 17:54:58 +0100

    add an early sample of CAP217

--- hiv_clinical/resources/samples/download_samples.sh
+++ hiv_clinical/resources/samples/download_samples.sh
@@ -20,4 +20,10 @@ mv week_112/raw_data/SRR9588774_1.fastq week_112/raw_data/SRR9588774_R1.fastq
 mv week_112/raw_data/SRR9588774_2.fastq week_112/raw_data/SRR9588774_R2.fastq
 
 
+fastq-dump -O CAP217/week_10/raw_data --split-e  SRR9588770
+mv CAP217/week_10/raw_data/SRR9588770_1.fastq CAP217/week_10/raw_data/SRR9588770_R1.fastq
+mv CAP217/week_10/raw_data/SRR9588770_2.fastq CAP217/week_10/raw_data/SRR9588770_R2.fastq
+
+
+
 fastq-dump -O philippines/pX/raw_data --split-e  SRR3608622
