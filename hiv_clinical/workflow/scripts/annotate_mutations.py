4d4b6b3213cf720f61f9114bd97ec45fce45feb8
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-01-10 16:33:19 +0100

    fix

--- hiv_clinical/workflow/scripts/annotate_mutations.py
+++ hiv_clinical/workflow/scripts/annotate_mutations.py
@@ -98,9 +98,9 @@ def main(fname_all_mutations, fname_hxb2_annotations, fname_genes, fname_all_mut
                 if position in postition_range:
                     aa_pos = (position - int(item["refRanges"][0][0])) % 3
                     return gene, aa_pos+1
-        return "error"
+        return "error", "error"
 
-    df['gene'], df['aa_position'] = df["Pos"].apply(get_gene)
+    df[['gene', 'aa_position']]  = df["Pos"].apply(get_gene).apply(pd.Series)
 
     df.to_csv(fname_all_mutations_annotated)
 
