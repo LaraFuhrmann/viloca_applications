0100eefba37c65df80b111665662809fed8e0876
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-01-15 10:37:44 +0100

    add another sewage sample

--- polio_sewage/resources/samples/download_samples.sh
+++ polio_sewage/resources/samples/download_samples.sh
@@ -3,5 +3,8 @@
 module load sra-toolkit/2.10.5
 
 fastq-dump -O pakistan/ES-03/raw_data --split-e  ERR4033233
+fastq-dump -O pakistan/ES-06/raw_data --split-e  ERR4033236
+
 
 fastq-dump -O pakistan/Sabin1_PanPV/raw_data --split-e  ERR4027774
+	
