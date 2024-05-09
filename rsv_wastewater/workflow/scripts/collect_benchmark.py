813c8ea207fb3d58438e107d1950de104ff12396
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-05-09 11:07:25 +0200

    collect benchmark files

---
+++ rsv_wastewater/workflow/scripts/collect_benchmark.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def main(fnames, samples, fname):
+
+    tmp = []
+
+    for file, sample in zip(fnames, samples):
+        sample = sample.split('results/')[1]
+        df = pd.read_csv(file, sep='\t')
+        df['sample'] = sample
+
+        tmp.append(df)
+
+    pd.concat(tmp).to_csv(fname)
+
+
+if __name__ == "__main__":
+    main(
+        snakemake.input.fnames,
+        snakemake.params.samples,
+        snakemake.output.fname,
+    )
