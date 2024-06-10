732c9eb59a2b8e18d7c4236d07cccfc687c6bdee
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-06-10 10:39:55 +0200

    correct dataframe

--- rsv_wastewater/workflow/scripts/collect_coverage.py
+++ rsv_wastewater/workflow/scripts/collect_coverage.py
@@ -8,7 +8,7 @@ def main(fnames_coverage, samples, fname_all_coverage):
     tmp = []
 
     for file, sample in zip(fnames_coverage, samples):
-        sample = sample.split('results/')[1]
+        sample = sample.split('results/')
         df = pd.read_csv(file, sep='\t')
         df['coverage'] = df[sample]
         df['sample'] = sample
